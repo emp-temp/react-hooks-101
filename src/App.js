d47aba1d6be657883fb2330313bd3844065b05d3
寺尾空 <empty@teraokuus-MacBook-Air-4.local>
2022-08-18 11:45:38 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  const x2 = () => {
+    setCount(count * 2)
+  }
+
+  const is3 = () => {
+    if (count % 3 === 0) {
+      setCount(count / 3);
+    }
+  }
+
+  const divide3 = () => setCount(previousCount =>
+    previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  )
+
   return (
-    <div>
-    This is a template for React app.
-    </div>
+    <>
+      <div>
+        count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+        <button onClick={x2}>x2</button>
+        <button onClick={is3}>is3</button>
+        <button onClick={divide3}>divide3</button>
+      </div>
+    </>
   );
 }
 
